8797f2ddc69ac3e969614bad498cf8b2e7c95ec0
Jonas <71136545+Jonas-Zelinka@users.noreply.github.com>
2020-09-11 17:26:22 +0200

    Update high-level-IO.c

--- high-level-IO.c
+++ high-level-IO.c
@@ -9,12 +9,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#define LINEBUFF 256
-#define NAME 200
+#define LINEBUFF 256	//define buffer length for one line
+#define NAME 200	//define buffer length for one Name
 //////////////////////////////////////////////////////////
 //	Void´s						//
 //////////////////////////////////////////////////////////
-static void error_exit( char *errormessage ) {
+static void error_exit( char *errormessage ) {	// error checking function
     fprintf (stderr, "\n %s ; %s \n", errormessage, strerror(errno));
     exit(EXIT_FAILURE); }
 //////////////////////////////////////////////////////////
@@ -22,28 +22,28 @@ static void error_exit( char *errormessage ) {
 //////////////////////////////////////////////////////////
 int main( int argc, char *argv[] )
 {//   MAIN START
-char buff[LINEBUFF];
+char buff[LINEBUFF]; // declare buffer for one hole Line
 
-char name[NAME],fname[NAME];
-int day,month,year;
-FILE *fd;
-char path[]=".test.txt";
-fd= fopen(path,"r");
+char name[NAME],fname[NAME]; // declare name buffer and second name buffer
+int day,month,year;		// declare int types for date,
+FILE *fd;	//file descriptor
+char path[]=".test.txt";	// path to file...
+fd= fopen(path,"r");		// open file with "Read" permissions
 // read line by line till end
-printf("\n");
-while(fgets(buff,LINEBUFF,fd) != NULL)
+printf("\n"); // space
+while(fgets(buff,LINEBUFF,fd) != NULL) // write line by line in buff with length="LINEBUFF" from file descriptor "fd" till the end of file "!= NULL"
 	{
-	printf("DATA: %s",buff);
+	printf("DATA: %s",buff); // Print one hole line (buff)
 	}
-fclose(fd);
+fclose(fd); // close file descriptor
 
-fd= fopen(path,"r");
+fd= fopen(path,"r"); // open file descriptor again with "Read" permissions
 // read word by word
 printf("\n now read word by word ...\n");
 while( fscanf(fd,"%d %d %d %s %s",&month,&day,&year,&name,&fname) != -1) // print till end
-	{
-	printf("DATA: %02d %02d %04d %s %s\n",month,day,year,name,fname);
+	{	// copie every word in a diffrent buffer (perfekt for Date programms and so on)
+	printf("DATA: %02d %02d %04d %s %s\n",month,day,year,name,fname); // print Date
 	}
-fclose(fd);
-printf("\n");
+fclose(fd);	// close file descriptor
+printf("\n");	// space
 }//   MAIN END
